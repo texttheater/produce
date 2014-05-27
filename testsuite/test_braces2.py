5a218a30b04a5e7f6789b7cd4858e14c796bcba8
Kilian Evang <noreply@texttheater.net>
2014-05-27 15:34:46 +0200

    Added additional test for braces, inspired by #2.

---
+++ testsuite/test_braces2.py
@@ -0,0 +1,15 @@
+import os
+
+from prodtest import ProduceTestCase
+from produce import ProduceError
+
+class BracesTest2(ProduceTestCase):
+
+    """
+    Tests using } in Python expressions.
+    """
+
+    def test_braces2(self):
+        with self.assertRaisesRegex(ProduceError,
+                r'no rule to produce 1/1\.aux'):
+            self.produce('test.pdf')
