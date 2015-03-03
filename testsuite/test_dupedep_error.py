70f5c62377a8ff807c1862b0f6b6fad0f6106c6e
Kilian Evang <noreply@texttheater.net>
2015-03-03 13:30:58 +0100

    Added tests against duplicate recipe execution.

---
+++ testsuite/test_dupedep_error.py
@@ -0,0 +1,18 @@
+import logging
+import prodtest
+import produce
+
+class DudedepErrorTest(prodtest.ProduceTestCase):
+
+    """
+    Tests that duplicate dependencies do not lead to redundant recipe
+    execution.
+    """
+
+    def test(self):
+        self.assertDirectoryContents(['produce.ini'])
+        with self.assertLogs(level='INFO') as l:
+            with self.assertRaises(produce.ProduceError):
+                self.produce()
+        self.assertEqual(len(l.output), 1) # for b, which fails
+        self.assertDirectoryContents(['produce.ini'])
---
+++ testsuite/test_dupedep_success.py
@@ -0,0 +1,16 @@
+import logging
+import prodtest
+
+class DudedepSuccessTest(prodtest.ProduceTestCase):
+
+    """
+    Tests that duplicate dependencies do not lead to redundant recipe
+    execution.
+    """
+
+    def test(self):
+        self.assertDirectoryContents(['produce.ini'])
+        with self.assertLogs(level='INFO') as l:
+            self.produce()
+        self.assertEqual(len(l.output), 3) # for a, b, y
+        self.assertDirectoryContents(['produce.ini', 'a', 'b', 'y'])
