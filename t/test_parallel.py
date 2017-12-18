f0acef8583bf3371472efd21d169e2bbdc95b055
Kilian Evang <noreply@texttheater.net>
2017-12-19 00:43:59 +0100

    Run test_parallel 10x faster

--- t/test_parallel.py
+++ t/test_parallel.py
@@ -9,20 +9,18 @@ class ParallelNoDepsTest(prodtest.ProduceTestCase):
         start = time.time()
         self.produce(**{'-f': 'nodeps.ini'})
         stop = time.time()
-        # The "Equal" is for systems where precision is only a second. Do these
-        # still exist? Gosh, I hope not.
-        self.assertGreaterEqual(stop - start, 9)
+        self.assertGreater(stop - start, 0.9)
         start = time.time()
         self.produce(**{'-f': 'nodeps.ini', '-j': '4'})
         stop = time.time()
-        self.assertLessEqual(stop - start, 6)
+        self.assertLess(stop - start, 0.6)
         # Now, three targets where one depends on the two others. Should take
         # ~6s to run sequentially, ~4s with four threads.
         start = time.time()
         self.produce(**{'-f': 'deps.ini'})
         stop = time.time()
-        self.assertGreaterEqual(stop - start, 6)
+        self.assertGreater(stop - start, 0.6)
         start = time.time()
         self.produce(**{'-f': 'deps.ini', '-j': '4'})
         stop = time.time()
-        self.assertLessEqual(stop - start, 5)
+        self.assertLess(stop - start, 0.5)
