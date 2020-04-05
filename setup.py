a64d7692bf622f9552932caf207146d25a26c23e
Kilian Evang <noreply@texttheater.net>
2020-04-05 18:32:54 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
     name="produce",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     scripts=['produce'],
 
     # metadata to display on PyPI
     author="Kilian Evang",
-    author_email="kilian@evang.name",
+    author_email="kilian.evang@gmail.com",
     description="Replacement for Make geared towards processing data rather than compiling code",
     license="MIT",
     keywords="make builder automation",
