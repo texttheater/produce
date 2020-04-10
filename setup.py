cd6cf838ddd1a5ba3a76cdf6bacac997ad266e0d
Loïc Grobol <loic.grobol@gmail.com>
2020-04-10 15:29:19 +0200

    move meta data to setup.cfg and add long desc

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from setuptools import setup, find_packages
-setup(
-    name="produce",
-    version="0.2",
-    packages=find_packages(),
-    scripts=['produce'],
+from setuptools import setup
 
-    # metadata to display on PyPI
-    author="Kilian Evang",
-    author_email="kilian.evang@gmail.com",
-    description="Replacement for Make geared towards processing data rather than compiling code",
-    license="MIT",
-    keywords="make builder automation",
-    url="https://github.com/texttheater/produce", 
-)
+setup()
