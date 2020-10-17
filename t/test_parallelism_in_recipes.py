a64d5d30eeef7f83d8f159d1566b7cc61539f5ef
Kilian Evang <noreply@texttheater.net>
2020-10-17 20:44:39 +0200

    Increased tolerances in parallelism test

--- t/test_parallelism_in_recipes.py
+++ t/test_parallelism_in_recipes.py
@@ -6,7 +6,7 @@ class ParallelismTest(prodtest.ProduceTestCase):
     def test_parallelism(self):
         # control: no declared parallelism
         # recipes run in parallel
-        with self.assertTakesLessThan(0.25):
+        with self.assertTakesLessThan(0.3):
             self.produce('a', **{'-j': '2'})
         # treatment: declared parallelism
         # recipes run in sequence
@@ -14,5 +14,5 @@ class ParallelismTest(prodtest.ProduceTestCase):
             self.produce('d', **{'-j': '2'})
         # Make sure running with less parallelism than the recipe "needs" still
         # works:
-        with self.assertTakesLessThan(0.35):
+        with self.assertTakesLessThan(0.4):
             self.produce('d', **{'-j': '1', '-B': None})
